39232ca74f0bbecacb7116d8388d9003049888bb
jackwatk <jackwatk@hotmail.co.uk>
2018-12-14 09:33:17 +0100

    find all

--- src/pages/CreateGuide.js
+++ src/pages/CreateGuide.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import GuideService from '../lib/guides-service'
 class CreateGuide extends Component {
 
   state = {
@@ -13,8 +13,18 @@ class CreateGuide extends Component {
     duration: '',
   }
  
- 
+  onSubmit = (data) => {
+    GuideService.createGuide(data)
+        .then((response) => {
+          console.log("hello" + response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
     handleChange = (event) => {
+      event.preventDefault();
       this.setState({
         [event.target.name]: event.target.value,
       })
@@ -26,18 +36,18 @@ class CreateGuide extends Component {
       <section className="create-edit-section">
         <div className="create-edit-form-container">
         <h2>Create guide</h2>
-          <form action="/guides-list" method="POST" className="create-edit-form">
-            <input type="text" placeholder="Title" name="title" className="create-edit-input" onChange={this.handleChange} value={this.state.searchValue}/>
-            <input type="text" placeholder="Description" name="description" className="create-edit-input desc" onChange={this.handleChange} value={this.state.searchValue} />
-            <input type="text" placeholder="Location" name="location" className="create-edit-input" onChange={this.handleChange} value={this.state.searchValue} />
-            <input type="text" placeholder="Expertise" name="expertise" className="create-edit-input" onChange={this.handleChange} value={this.state.searchValue} />
-            <div className="duration-container" onChange={this.handleChange} value={this.state.searchValue}>
+          <form action="/guides-list/" method="POST" className="create-edit-form">
+            <input type="text" placeholder="Title" name="title" className="create-edit-input" onChange={this.handleChange} value={this.state.title}/>
+            <input type="text" placeholder="Description" name="description" className="create-edit-input desc" onChange={this.handleChange} value={this.state.description} />
+            <input type="text" placeholder="Location" name="location" className="create-edit-input" onChange={this.handleChange} value={this.state.location} />
+            <input type="text" placeholder="Expertise" name="expertise" className="create-edit-input" onChange={this.handleChange} value={this.state.expertise} />
+            <div className="duration-container">
               <p>Duration:</p>
-            <input type="text" placeholder="Date" name="date" className="create-edit-input" onChange={this.handleChange} value={this.state.searchValue} />
-            <input type="text" placeholder="Time" name="time" className="create-edit-input" onChange={this.handleChange} value={this.state.searchValue} />
-              <input type="time" placeholder="hh" name="duration" className="duration-input" onChange={this.handleChange} value={this.state.searchValue} />
+            <input type="text" placeholder="Date" name="date" className="create-edit-input" onChange={this.handleChange} value={this.state.date} />
+            <input type="text" placeholder="Time" name="time" className="create-edit-input" onChange={this.handleChange} value={this.state.time} />
+              <input type="time" placeholder="hh" name="duration" className="duration-input" onChange={this.handleChange} value={this.state.duration} />
             </div>
-            <button type="submit">Create</button>
+            <button type="submit" onSubmit={this.onSubmit}>Create</button>
           </form>
         </div>
       </section>
