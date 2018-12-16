1a4ab6a09e1ce9e28bba8bda1fadfdd290787ff3
jackwatk <jackwatk@hotmail.co.uk>
2018-12-16 18:28:12 +0100

    home page changes

---
+++ src/pages/HomeSick.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Scan extends Component {
+        state = {
+            cats: [],
+            dogs: {x: 0, y: 0},
+            loading: true,
+        }
+    
+    componentDidMount(){
+            let catList = []
+            
+            let randomNumber = 0
+            for(let i =0; i <6; i++){
+                let cat = {x:0, y:0}
+            randomNumber = Math.floor(Math.random() * 500);
+            cat.x = randomNumber + 75;
+            cat.y = randomNumber + 100;
+            catList.push(cat);
+            console.log(cat)
+        }
+        console.log(catList)
+    
+        this.setState({
+            cats: catList,
+            loading: false,
+        })
+
+    }
+  
+
+  
+
+  render() {
+    
+    const {cats} = this.state
+    return (
+      <section className="home-sick">
+        <div className='cat-container'>
+        {cats.map((cat, index) => {
+                return <img key={index} src={`https://placekitten.com/${cat.x}/${cat.y}`} />
+        })}
+        
+        </div>
+        
+     
+      </section>
+    );
+  }
+}
+
+export default Scan;
